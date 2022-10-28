61a9e1f6e7748ae21e5d6acab1e035ae2db83733
dsewardj <78860059+dsewardj@users.noreply.github.com>
2022-10-28 10:14:49 -0400

    Create bcp_script_generator.sql

---
+++ bcp_script_generator.sql
@@ -0,0 +1,16 @@
+declare @db_name nvarchar(255)
+declare @svr_name nvarchar(255)
+declare @filepath nvarchar(255)
+declare @args nvarchar(255)
+
+set @db_name = 'AdventureWorksDW2019'
+set @svr_name = 'DONNY-DESKTOP'
+set @filepath = 'C:\flat_files\'
+set @args = '-T -c'
+
+select 'bcp ' + @db_name + '.' + sch.name + '.' + tbl.name + ' out ' 
+  + @filepath + tbl.name + '.csv ' + '-S ' + @svr_name + ' ' + @args + ';' as bcp
+from sys.tables as tbl
+left join sys.schemas as sch
+ on sch.schema_id = tbl.schema_id
+;
